f32027a8ea15d7be5e008548c49ee8347fd7ca56
burnsi1996 <burnsi@iu.edu>
2023-06-30 15:21:45 -0400

    Doesn't Change

---
+++ labs/lab1/js/app.js
@@ -0,0 +1,17 @@
+function setup() {
+    var red = "#FF0000";
+    var startX = 30;
+    createCanvas(800,600);
+
+    fill(150,123,237);
+    circle(100,100, 30);
+    stroke(187,237,220);
+    noStroke();
+    background(70,60, 300, 50,);
+
+    fill(207,123,237);
+    rect(70, 60, 300, 50);
+
+    stroke(2);
+    line(10, 20, 200, 20)
+}
