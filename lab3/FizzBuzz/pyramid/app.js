da862972f2566db0585cc09789ea6754c95bbfe1
burnsi1996 <burnsi@iu.edu>
2023-07-07 23:08:59 -0400

    adding coding to app.js

--- lab3/FizzBuzz/pyramid/app.js
+++ lab3/FizzBuzz/pyramid/app.js
@@ -1,12 +1,8 @@
-let listenToMusic = confirm("Do you want to listen to music?"); 
-let readTheLyrics = confirm("Do you also want to read the lyrics?") 
-if(listenToMusic == true) { 
-	//if the user wants to listen to music
-	if(readTheLyrics==true) { 
-		//if the user wants to read the lyrics
-		console.log("Excellent");
-	} 
-} else { 
-	//if the user does not want to listen to music 
-	console.log("You'll be Bored!"); 
-} 
+function setup() {
+    createCanvas(800, 600);
+
+}
+
+function draw() {
+    
+}
